de263eb414f47b6647f4ff2db5d5f14c9dcf21b5
owl1234 <34064188+owl1234@users.noreply.github.com>
2020-08-15 23:43:42 +0300

    Add files via upload

---
+++ project.py
@@ -0,0 +1,468 @@
+import os
+import pygame
+import sys
+import random
+import time
+import math
+
+pygame.init()
+pygame.mouse.set_visible(0)
+size = width, height = 800, 600
+screen = pygame.display.set_mode(size)
+clock = pygame.time.Clock()
+
+pygame.key.set_repeat(200, 10)
+fps = 50
+
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join('data', name)
+    try:
+        image = pygame.image.load(fullname)
+    except pygame.error as message:
+        print('Cannot load image:', name)
+        raise SystemExit(message)
+
+    image = image.convert_alpha()
+    if colorkey is not None:
+        if colorkey is -1:
+            colorkey = image.get_at((-10000, 0))
+        image.set_colorkey(colorkey)
+    return image
+
+
+def terminate():
+    pygame.quit()
+    sys.exit()
+
+
+# основной персонаж
+player = None
+speed = 4
+# группы спрайтов
+all_sprites = pygame.sprite.Group()
+particles_sprites = pygame.sprite.Group()
+indicator_sprites = pygame.sprite.Group()
+tiles_group = pygame.sprite.Group()
+koryaga_group = pygame.sprite.Group()
+grass_group = pygame.sprite.Group()
+fish_group = pygame.sprite.Group()
+player_group = pygame.sprite.Group()
+count = 0
+tile_images = {'water': load_image('not.png')}
+koryaga_image = load_image('koryagaa.png')
+grass_image = load_image('grasss.png')
+player_image_nach = load_image('catt.png')
+player_image_right = load_image('catt1.png')
+player_image_left = load_image('catt.png')
+fish_image = load_image('fishh.png')
+tile_width = 49
+tile_height = 70
+koryaga_width = koryaga_height = 50
+grass_width = grass_height = 50
+fish_width = 100
+fish_height = 82
+indicator_width = indicator_height = 100
+
+
+class koryaga(pygame.sprite.Sprite):
+    def __init__(self, posx, posy):
+        super().__init__(koryaga_group, all_sprites)
+        self.image = load_image('koryaga.png')
+        self.rect = self.image.get_rect().move(koryaga_width * posx + 20, koryaga_height * posy - 20)
+        self.mask = pygame.mask.from_surface(self.image)
+
+    def update(self):
+        global speed
+        global time_old
+        if pygame.sprite.collide_mask(self, player):
+            speed = 2
+            time_old = time.time()
+
+
+class grass(pygame.sprite.Sprite):
+    def __init__(self, posx, posy):
+        super().__init__(grass_group, all_sprites)
+        self.image = load_image('grasss.png')
+        self.rect = self.image.get_rect().move(grass_width * posx + 20, grass_height * posy - 20)
+        self.mask = pygame.mask.from_surface(self.image)
+
+    def update(self):
+        global speed
+        global time_old
+        if pygame.sprite.collide_mask(self, player):
+            speed = 6
+            time_old = time.time()
+
+
+def startScreen():
+    music = pygame.mixer.music.load("data/sea.mp3")
+    pygame.mixer.music.play(-1)
+    introText = ["В этой игре вы можете почувствовать себя настоящим котом.",
+                 "Цель игры - найти рыбу на поле."]
+
+    screen.fill(pygame.Color('black'))
+    font = pygame.font.Font(None, 50)
+    stringRendered = font.render("Поймай рыбку!", 1, (100, 255, 100))
+    introRect = stringRendered.get_rect()
+    introRect.top = 20
+    introRect.x = 250
+    screen.blit(stringRendered, introRect)
+
+    font = pygame.font.Font(None, 30)
+    textCoord = 80
+    for line in introText:
+        stringRendered = font.render(line, 1, (100, 255, 100))
+        introRect = stringRendered.get_rect()
+        textCoord += 10
+        introRect.top = textCoord
+        introRect.x = 20
+        textCoord += introRect.height
+        screen.blit(stringRendered, introRect)
+
+    textCoord += 20
+    introText = ["Правила игры просты. На поле прячется рыба, которую кот должен поймать.",
+                 "По пути он может встретить корягу, которая при касании с ним уменьшает",
+                 "его скорость, и траву, при касании с которой скорость увеличивается.",
+                 "В левом нижнем углу есть индикатор, показывающий удалённость",
+                 "рыбы от персонажа. Чем он темнее, тем дальше кот от цели.",
+                 "Соответственно, чем светлее, тем ближе. Управление котом происходит",
+                 "при помощи стрелок на клавиатуре. А вы сможете поймать рыбку?"]
+    for line in introText:
+        stringRendered = font.render(line, 1, (100, 255, 100))
+        introRect = stringRendered.get_rect()
+        textCoord += 10
+        introRect.top = textCoord
+        introRect.x = 20
+        textCoord += introRect.height
+        screen.blit(stringRendered, introRect)
+
+    ray = load_image('ray.png')  # луч света
+    ray1 = pygame.transform.scale(ray, (186, 186))
+    screen.blit(ray1, [370, 400])
+
+    cat_begin = load_image('begin.png')
+    cat1 = pygame.transform.scale(cat_begin, (149, 148))
+    screen.blit(cat1, [400, 410])
+
+    global count
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+            elif event.type == pygame.KEYDOWN or event.type == pygame.MOUSEBUTTONDOWN:
+                return  # начинаем игру
+        pygame.display.flip()
+        count += 1
+        if count % 500 == 0:
+            for i in range(random.randint(1, 10)):
+                grass(random.randint(0, 80), random.randint(0, 80))
+
+            for i in range(random.randint(1, 10)):
+                koryaga(random.randint(0, 80), random.randint(0, 80))
+            count = 0
+        clock.tick(fps)
+
+
+startScreen()
+all_time = time.time()
+screen_rect = (0, 0, width, height)
+
+
+def endScreen():
+    music = pygame.mixer.music.load("data/victory.mp3")
+    pygame.mixer.music.play(-1)
+    user_time = int(time.time() - all_time)
+    ut_min = user_time // 60
+    ut_sec = user_time - ut_min * 60
+    text = "Вы поймали рыбу за"
+    if ut_min > 0:
+        text += " " + str(ut_min)
+        if ut_min > 10 and ut_min < 20:
+            text += " минут"
+        elif ut_min % 10 == 1:
+            text += " минуту"
+        elif ut_min % 10 == 2 or ut_min % 10 == 3 or ut_min % 10 == 4:
+            text += " минуты"
+        else:
+            text += " минут"
+
+    if ut_sec != 0:
+        text += " " + str(ut_sec)
+        if ut_sec > 10 and ut_sec < 20:
+            text += " секунд"
+        elif ut_sec % 10 == 1:
+            text += " секунду"
+        elif ut_sec % 10 == 2 or ut_sec % 10 == 3 or ut_sec % 10 == 4:
+            text += " секунды"
+        else:
+            text += " секунд"
+
+    text += "!"
+    font = pygame.font.Font(None, 50)
+    text = font.render(text, 1, (100, 255, 100))
+
+    screen.blit(text, [50, 100])
+
+    while True:
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+        screen.fill((0, 0, 0))
+        create_particles([random.randint(150, 250), random.randint(450, 550)])
+        create_particles([random.randint(550, 650), random.randint(450, 550)])
+        particles_sprites.draw(screen)
+        particles_sprites.update()
+        screen.blit(text, [50, 100])
+        pygame.display.flip()
+        clock.tick(fps)
+
+
+class Particle(pygame.sprite.Sprite):
+    # сгенерируем частицы разного размера
+    fire = []
+    image_star = load_image("star.png")
+    fire.append(pygame.transform.scale(image_star, (20, 20)))
+    fire.append(pygame.transform.scale(image_star, (10, 10)))
+
+    def __init__(self, x, y, dx, dy, size):
+        super().__init__(particles_sprites)
+
+        self.image = Particle.fire[random.randint(0, len(Particle.fire) - 1)]
+
+        self.rect = self.image.get_rect()
+
+        # у каждой частицы своя скорость
+        self.x_velocity = dx
+        self.y_velocity = dy
+
+        self.rect.x = x
+        self.rect.y = y
+
+        # гравитация
+        self.gravity = 0.1
+
+    def update(self):
+        # применяем гравитационный эффект
+        self.y_velocity += self.gravity
+        # перемещаем частицу
+        self.rect.x += self.x_velocity
+        self.rect.y += self.y_velocity
+        # убиваем, если частица ушла за экран
+        if not self.rect.colliderect(screen_rect):
+            self.kill()
+
+
+def create_particles(position):
+    # количество создаваемых частиц
+    particle_count = 20
+
+    # возможные скорости
+    numbers = list(range(-5, 5))
+
+    for i in range(0, particle_count):
+        p = Particle(position[0], position[1], random.choice(numbers), random.choice(numbers), random.randint(1, 5))
+
+
+def load_level(filename):
+    filename = "data/" + filename
+    # читаем уровень, убирая символы перевода строки
+    with open(filename, 'r') as mapFile:
+        level_map = [line.strip() for line in mapFile]
+
+    # и подсчитываем максимальную длину
+    maxWidth = max(map(len, level_map))
+
+    # дополняем каждую строку пустыми клетками ('.')
+    return list(map(lambda x: x.ljust(maxWidth, '.'), level_map))
+
+
+class Indicator(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__(indicator_sprites)
+        self.image = load_image('1.png')
+        self.rect = self.image.get_rect()
+        self.rect.x = 10
+        self.rect.y = 490
+
+    def update(self):
+        min_len_to_fish = 10000000000
+        for fish in fish_group:
+            if ((player.rect.x - fish.rect.x) ** 2 + (player.rect.y - fish.rect.y) ** 2) ** 0.5 < min_len_to_fish:
+                min_len_to_fish = ((player.rect.x - fish.rect.x) ** 2 + (player.rect.y - fish.rect.y) ** 2) ** 0.5
+
+        if min_len_to_fish < 200:
+            self.image = load_image('16.png')
+        elif min_len_to_fish < 400:
+            self.image = load_image('15.png')
+        elif min_len_to_fish < 600:
+            self.image = load_image('14.png')
+        elif min_len_to_fish < 800:
+            self.image = load_image('13.png')
+        elif min_len_to_fish < 1000:
+            self.image = load_image('12.png')
+        elif min_len_to_fish < 1200:
+            self.image = load_image('11.png')
+        elif min_len_to_fish < 1400:
+            self.image = load_image('10.png')
+        elif min_len_to_fish < 1600:
+            self.image = load_image('9.png')
+        elif min_len_to_fish < 1800:
+            self.image = load_image('8.png')
+        elif min_len_to_fish < 2000:
+            self.image = load_image('7.png')
+        elif min_len_to_fish < 2200:
+            self.image = load_image('6.png')
+        elif min_len_to_fish < 2400:
+            self.image = load_image('5.png')
+        elif min_len_to_fish < 2500:
+            self.image = load_image('4.png')
+        elif min_len_to_fish < 2600:
+            self.image = load_image('3.png')
+        elif min_len_to_fish < 2900:
+            self.image = load_image('2.png')
+        else:
+            self.image = load_image('1.png')
+
+
+class Tile(pygame.sprite.Sprite):
+    def __init__(self, type, posx, posy):
+        super().__init__(tiles_group, all_sprites)
+        self.image = tile_images[type]
+        self.rect = self.image.get_rect().move(tile_width * posx, tile_height * posy)
+
+
+class Fish(pygame.sprite.Sprite):
+    def __init__(self, posx, posy):
+        super().__init__(fish_group, all_sprites)
+        self.image = fish_image
+        self.rect = self.image.get_rect().move(fish_width * posx, fish_height * posy)
+        self.mask = pygame.mask.from_surface(self.image)
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, posx, posy):
+        super().__init__(player_group, all_sprites)
+        self.count = 0
+        self.image = player_image_nach
+        self.rect = self.image.get_rect().move(tile_width * posx + 15, tile_height * posy + 5)
+
+    def update(self):
+        if pygame.sprite.spritecollideany(self, fish_group):
+            for i in pygame.sprite.spritecollide(self, fish_group, 0):
+                screen.fill(pygame.Color(0, 0, 0))
+                pygame.display.flip()
+                endScreen()
+
+
+def generate_level(level):
+    for y in range(len(level)):
+        for x in range(len(level[y])):
+            Tile('water', x, y)
+            if level[y][x] == '@':
+                player = Player(x, y)
+            elif level[y][x] == '!':
+                Fish(x, y)
+            elif level[y][x] == 'g':
+                grass(x, y)
+            elif level[y][x] == 'k':
+                koryaga(x, y)
+    return player, x, y
+
+
+class Camera():
+    # зададим начальный сдвиг камеры и размер поля для возможности реализации циклического сдвига
+    def __init__(self, fieldsize):
+        self.dx = 0
+        self.dy = 0
+        self.fieldsize = fieldsize
+
+    # сдвинуть объект obj на смещение камеры
+    def apply(self, obj):
+        obj.rect.x += self.dx
+        # вычислим координату клетки, если она уехала влево за границу экрана
+        if obj.rect.x < -obj.rect.width:
+            obj.rect.x += (self.fieldsize[0] + 1) * obj.rect.width
+        # вычислим координату клетки, если она уехала вправо за границу экрана
+        if obj.rect.x >= (self.fieldsize[0]) * obj.rect.width:
+            obj.rect.x += -obj.rect.width * (1 + self.fieldsize[0])
+        obj.rect.y += self.dy
+        # вычислим координату клетки, если она уехала вверх за границу экрана
+        if obj.rect.y < -tile_height:
+            obj.rect.y += (self.fieldsize[1] + 1) * tile_height
+        # вычислим координату клетки, если она уехала вниз за границу экрана
+        if obj.rect.y >= (self.fieldsize[1]) * tile_height:
+            obj.rect.y += -tile_height * (1 + self.fieldsize[1])
+
+    # позиционировать камеру на объекте target
+    def update(self, target):
+        self.dx = -(target.rect.x + target.rect.w // 2 - width // 2)
+        self.dy = -(target.rect.y + target.rect.h // 2 - height // 2)
+
+
+player, levelx, levely = generate_level(load_level("file.txt"))
+camera = Camera((levelx, levely))
+indicator = Indicator()
+
+time_old = 0
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                player.image = player_image_left
+                player.rect.x -= speed
+            if event.key == pygame.K_RIGHT:
+                player.image = player_image_right
+                player.rect.x += speed
+            if event.key == pygame.K_UP:
+                player.rect.y -= speed
+            if event.key == pygame.K_DOWN:
+                player.rect.y += speed
+
+    # изменяем положение камеры
+    screen.fill(pygame.Color(0, 0, 0))
+    indicator.update()
+
+    camera.update(player)
+    # обновляем положение всех спрайтов
+    for sprite in all_sprites:
+        camera.apply(sprite)
+
+    player.update()
+
+    for i in grass_group:
+        i.update()
+    for i in koryaga_group:
+        i.update()
+
+    backgr = load_image('water.gif')
+    backgr1 = pygame.transform.scale(backgr, (800, 600))
+    screen.blit(backgr1, [0, 0])
+
+    koryaga_group.draw(screen)
+    grass_group.draw(screen)
+    fish_group.draw(screen)
+    player_group.draw(screen)
+    indicator_sprites.draw(screen)
+
+    if time.time() - time_old >= 5:
+        speed = 4
+
+    pygame.display.flip()
+
+    clock.tick(fps)
+
+    count += 1
+    if count % 500 == 0:
+        for i in range(random.randint(1, 15)):
+            grass(random.randint(0, 80), random.randint(0, 80))
+
+        for i in range(random.randint(1, 15)):
+            koryaga(random.randint(0, 80), random.randint(0, 80))
+        count = 0
+
+terminate()
